1f138188e07b72c42bd9ed8e53500244435fbda2
Jacques Roux <jacquesf.roux@gmail.com>
2024-02-01 16:20:46 +0200

    Refactor database class & configurations

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ require 'functions.php';
 // require 'router.php';
 require 'Database.php';
 
+$config = require 'config.php';
+$db = new Database($config['database'], 'root', 'karools');
 
-$db = new Database();
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id";
 
+$posts = $db->query($query, [':id' => $id])->fetch();
 
-foreach ($posts as $post) {
-  echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);

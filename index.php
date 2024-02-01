cd311239eeeaf504743e59188b937221d67ab200
Jacques Roux <jacquesf.roux@gmail.com>
2024-02-01 06:35:31 +0200

    Add initial router & PDO connection to mysql database

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
 require 'functions.php';
+// require 'router.php';
 
-$heading = 'Home';
+// Connect to our MySQL database
+$dsn = "mysql:host=localhost;port=3306;dbname=laracasts_myapp;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root', '');
 
-require 'views/index.view.php';
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['title'] . "</li>";
+}

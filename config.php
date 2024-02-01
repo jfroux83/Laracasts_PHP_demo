e0d58fa7de9551865fad83e963302b24da1e861a
Jacques Roux <jacquesf.roux@gmail.com>
2024-02-01 17:09:48 +0200

    Start adding notes section, db connection, controllers and views

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
   'database' => [
     'host' => 'localhost',
     'port' => 3306,
-    'dbname' => 'laracasts_myapp',
+    'dbname' => 'laracasts_notes',
     'charset' => 'utf8mb4'
   ],
 

e702c454fa98d5b49ce9fa7c6ba7a8426a6ff7c3
Jacques Roux <jacquesf.roux@gmail.com>
2024-02-02 09:06:47 +0200

    Refactor the Database class to include methods for get, find and findOrFail.

--- controllers/note.php
+++ controllers/note.php
@@ -4,7 +4,13 @@ $config = require 'config.php';
 $db = new Database($config['database'], 'root', 'karools');
 
 $heading = 'Note';
+$currentUserId = 1;
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", [':id' => $_GET['id']])->fetch();
+$note = $db->query(
+  "SELECT * FROM notes WHERE id = :id",
+  [':id' => $_GET['id']]
+)->findOrFail();
+
+authorize($note['user_id'] == $currentUserId);
 
 require 'views/note.view.php';
--- controllers/notes.php
+++ controllers/notes.php
@@ -5,6 +5,6 @@ $db = new Database($config['database'], 'root', 'karools');
 
 $heading = 'My Notes';
 
-$notes = $db->query("SELECT * FROM notes WHERE user_id = 1")->fetchAll();
+$notes = $db->query("SELECT * FROM notes WHERE user_id = 1")->get();
 
 require 'views/notes.view.php';

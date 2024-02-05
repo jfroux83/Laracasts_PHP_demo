575a385d25933c5803f17f8809309c86ef6a08a7
Jacques Roux <jacquesf.roux@gmail.com>
2024-02-05 17:44:39 +0200

    Build a new Router class that can handle multiple methods

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -8,6 +8,13 @@
       <a href="/notes" class="text-blue-500 underline">go back...</a>
     </p>
     <p><?= htmlspecialchars($note['body']) ?></p>
+
+    <form method="POST" class="mt-6">
+      <input type="hidden" name="_method" value="DELETE">
+      <input type="hidden" name="id" value="<?= $note['id'] ?>">
+      <button class="text-sm text-red-500">Delete</button>
+    </form>
+
   </div>
 </main>
 

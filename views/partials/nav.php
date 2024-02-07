47ceabd036b50a9e00acf6e340f6470f5fd2b101
Jacques Roux <jacquesf.roux@gmail.com>
2024-02-07 16:24:15 +0200

    Added login and logout functionality

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -10,7 +10,9 @@
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="/" class="<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
             <a href="/about" class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-            <a href="/notes" class="<?= urlIs('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Notes</a>
+            <?php if ($_SESSION['user'] ?? false) : ?>
+              <a href="/notes" class="<?= urlIs('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Notes</a>
+            <?php endif; ?>
             <a href="/contact" class="<?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
           </div>
         </div>
@@ -33,9 +35,14 @@
                 <span class="sr-only">Open user menu</span>
               </button>
               <?php if ($_SESSION['user']) : ?>
-                <p class="text-white"><?= $_SESSION['user']['email'] ?></p>
+                <!-- <p class="text-white"><?= $_SESSION['user']['email'] ?></p> -->
+                <form method="POST" action="/session">
+                  <input type="hidden" name="_method" value="DELETE" />
+                  <button class="text-white">Logout</button>
+                </form>
               <?php else : ?>
-                <a href="/register" class="text-white">Register</a>
+                <a href="/register" class="<?= urlIs('/register') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Register</a>
+                <a href="/login" class="<?= urlIs('/login') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
               <?php endif; ?>
             </div>
 

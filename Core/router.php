985476d0eb874adc13fccc7b49302ce1f39c77ba
Jacques Roux <jacquesf.roux@gmail.com>
2024-02-08 05:53:05 +0200

    Create a LoginForm validate class and refactor the file structure.

--- Core/router.php
+++ Core/router.php
@@ -74,7 +74,7 @@ class Router
 
         Middleware::resolve($route['middleware']);
 
-        require base_path($route['controller']);
+        require base_path('Http/controllers/' . $route['controller']);
       }
     }
 
